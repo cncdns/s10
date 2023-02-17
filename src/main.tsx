db4e5145a22e44920b14db8d14e512860cbc4597
cncdns <101331003+cncdns@users.noreply.github.com>
2023-02-17 20:43:00 +0800

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    {/* <a target="_blank" href="https://github.com/Xhofe">Github</a> */}
+  </React.StrictMode>,
+  document.getElementById('root')
+)
